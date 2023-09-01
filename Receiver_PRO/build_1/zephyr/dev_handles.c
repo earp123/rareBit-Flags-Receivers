6d3fca190f40e89a712b353e39368d49d000c601
earp123 <rallsw123@gmail.com>
2023-09-01 18:18:58 -0500

    Upload Receiver PRO Firmware

---
+++ Receiver_PRO/build_1/zephyr/dev_handles.c
@@ -0,0 +1,57 @@
+#include <zephyr/device.h>
+#include <zephyr/toolchain.h>
+
+/* 1 : /soc/clock@40000000:
+ */
+const Z_DECL_ALIGN(device_handle_t) __attribute__((__section__(".__device_handles_pass2")))
+__devicehdl_dts_ord_74[] = { DEVICE_HANDLE_SEP, DEVICE_HANDLE_SEP, DEVICE_HANDLE_ENDS };
+
+/* 2 : /soc/random@4000d000:
+ */
+const Z_DECL_ALIGN(device_handle_t) __attribute__((__section__(".__device_handles_pass2")))
+__devicehdl_dts_ord_98[] = { DEVICE_HANDLE_SEP, DEVICE_HANDLE_SEP, DEVICE_HANDLE_ENDS };
+
+/* 3 : /entropy_bt_hci:
+ */
+const Z_DECL_ALIGN(device_handle_t) __attribute__((__section__(".__device_handles_pass2")))
+__devicehdl_dts_ord_5[] = { DEVICE_HANDLE_SEP, DEVICE_HANDLE_SEP, DEVICE_HANDLE_ENDS };
+
+/* 4 : /soc/crypto@5002a000:
+ */
+const Z_DECL_ALIGN(device_handle_t) __attribute__((__section__(".__device_handles_pass2")))
+__devicehdl_dts_ord_118[] = { DEVICE_HANDLE_SEP, DEVICE_HANDLE_SEP, DEVICE_HANDLE_ENDS };
+
+/* 5 : /soc/uart@40028000:
+ */
+const Z_DECL_ALIGN(device_handle_t) __attribute__((__section__(".__device_handles_pass2")))
+__devicehdl_dts_ord_114[] = { DEVICE_HANDLE_SEP, DEVICE_HANDLE_SEP, DEVICE_HANDLE_ENDS };
+
+/* 6 : /soc/uart@40002000:
+ */
+const Z_DECL_ALIGN(device_handle_t) __attribute__((__section__(".__device_handles_pass2")))
+__devicehdl_dts_ord_113[] = { DEVICE_HANDLE_SEP, DEVICE_HANDLE_SEP, DEVICE_HANDLE_ENDS };
+
+/* 7 : /soc/adc@40007000:
+ */
+const Z_DECL_ALIGN(device_handle_t) __attribute__((__section__(".__device_handles_pass2")))
+__devicehdl_dts_ord_72[] = { DEVICE_HANDLE_SEP, DEVICE_HANDLE_SEP, DEVICE_HANDLE_ENDS };
+
+/* 8 : /soc/pwm@4001c000:
+ */
+const Z_DECL_ALIGN(device_handle_t) __attribute__((__section__(".__device_handles_pass2")))
+__devicehdl_dts_ord_69[] = { DEVICE_HANDLE_SEP, DEVICE_HANDLE_SEP, DEVICE_HANDLE_ENDS };
+
+/* 9 : /soc/flash-controller@4001e000:
+ */
+const Z_DECL_ALIGN(device_handle_t) __attribute__((__section__(".__device_handles_pass2")))
+__devicehdl_dts_ord_120[] = { DEVICE_HANDLE_SEP, DEVICE_HANDLE_SEP, DEVICE_HANDLE_ENDS };
+
+/* 10 : /soc/qspi@40029000/mx25r6435f@0:
+ */
+const Z_DECL_ALIGN(device_handle_t) __attribute__((__section__(".__device_handles_pass2")))
+__devicehdl_dts_ord_129[] = { DEVICE_HANDLE_SEP, DEVICE_HANDLE_SEP, DEVICE_HANDLE_ENDS };
+
+/* 11 : /pwmleds:
+ */
+const Z_DECL_ALIGN(device_handle_t) __attribute__((__section__(".__device_handles_pass2")))
+__devicehdl_dts_ord_68[] = { DEVICE_HANDLE_SEP, DEVICE_HANDLE_SEP, DEVICE_HANDLE_ENDS };
---
+++ Receiver_PRO/src/main.c
@@ -0,0 +1,618 @@
+#include <zephyr/types.h>
+#include <stddef.h>
+#include <errno.h>
+#include <zephyr/kernel.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/sys/byteorder.h>
+
+#include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/bluetooth/conn.h>
+#include <zephyr/bluetooth/uuid.h>
+#include <zephyr/bluetooth/gatt.h>
+#include <bluetooth/gatt_dm.h>
+#include <bluetooth/scan.h>
+#include <paging_client.h>
+
+//For power manager
+#include <zephyr/device.h>
+#include <zephyr/init.h>
+#include <zephyr/pm/pm.h>
+#include <zephyr/pm/device.h>
+#include <zephyr/pm/policy.h>
+#include <soc.h>
+
+//nrfx GPIO driver
+#include <hal/nrf_gpio.h>
+// Devicetree + GPIO includes
+#include <nrfx_gpiote.h>
+#include <helpers/nrfx_gppi.h>
+#if defined(DPPI_PRESENT)
+#include <nrfx_dppi.h>
+#else
+#include <nrfx_ppi.h>
+#endif
+#include <zephyr/irq.h>
+
+// SMP-related code
+#include <zephyr/mgmt/mcumgr/transport/smp_bt.h>
+
+#ifdef CONFIG_MCUMGR_CMD_FS_MGMT
+#include <zephyr/device.h>
+#include <zephyr/fs/fs.h>
+
+#include <zephyr/fs/littlefs.h>
+#endif
+#ifdef CONFIG_MCUMGR_CMD_OS_MGMT
+#include "zephyr/mgmt/mcumgr/grp/os_mgmt/os_mgmt.h"
+#endif
+#ifdef CONFIG_MCUMGR_CMD_IMG_MGMT
+#include "zephyr/mgmt/mcumgr/grp/img_mgmt/img_mgmt.h"
+#endif
+#ifdef CONFIG_MCUMGR_CMD_STAT_MGMT
+#include "zephyr/mgmt/mcumgr/grp/stat_mgmt/stat_mgmt.h"
+#endif
+#ifdef CONFIG_MCUMGR_CMD_SHELL_MGMT
+#include "zephyr/mgmt/mcumgr/grp/shell_mgmt/shell_mgmt.h"
+#endif
+#ifdef CONFIG_MCUMGR_CMD_FS_MGMT
+#include "zephyr/mgmt/mcumgr/grp/fs_mgmt/fs_mgmt.h"
+#endif
+
+#define RECEIVER_BUTTON_PIN	DT_GPIO_PIN(DT_ALIAS(sw0), gpios)
+//USB Pin
+
+#define HAPTIC_MOTOR_PIN DT_GPIO_PIN(DT_ALIAS(led0), gpios)
+//RGB LED
+
+#define WORQ_THREAD_STACK_SIZE  4096
+#define WORKQ_PRIORITY   5
+
+// Define stack area used by workqueue thread
+static K_THREAD_STACK_DEFINE(my_stack_area, WORQ_THREAD_STACK_SIZE);
+
+// Define queue structure
+static struct k_work_q offload_work_q = {0};
+
+
+struct work_info {
+    struct k_work work;
+    char name[25];
+} my_work;
+
+//static struct bt_gatt_exchange_params mtu_exchange_params[CONFIG_BT_MAX_CONN];
+
+bool debounce_check = false;
+
+// TODO Need to modify default_conn to better handle multiple connections.
+//      Currently, only one of the connections are successfully
+//      unreferenced upon disconnection. The other connection reference
+//      remains and is causing issues trying to reconnect.
+static struct bt_conn *default_conn;
+static struct bt_pag_client pag_c;
+
+static uint8_t volatile conn_count;
+
+char NOT_CONNECTED[] = "FF:FF:FF:FF:FF:FF";
+
+char AR1_device[] = "FF:FF:FF:FF:FF:FF";
+char AR2_device[] = "FF:FF:FF:FF:FF:FF";
+
+// Bluetooth advertising data
+static const struct bt_data ad[] = {
+	BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
+	BT_DATA(BT_DATA_NAME_COMPLETE, DEVICE_NAME, DEVICE_NAME_LEN),
+};
+
+static const struct bt_data sd[] = {
+	BT_DATA_BYTES(BT_DATA_UUID128_ALL,
+				  0x84, 0xaa, 0x60, 0x74, 0x52, 0x8a, 0x8b, 0x86,
+				  0xd3, 0x4c, 0xb7, 0x1d, 0x1d, 0xdc, 0x53, 0x8d)};
+
+void start_advertising_dfu(void)
+{
+	int err;
+
+	// Start advertising
+	err = bt_le_adv_start(BT_LE_ADV_CONN, ad, ARRAY_SIZE(ad), sd, ARRAY_SIZE(sd));
+	if (err)
+	{
+		printk("Advertising start failed with err %d\n", err);
+		return;
+	}
+
+	printk("Bluetooth advertising started!\n");
+}
+
+static uint8_t notify_func(struct bt_pag_client *pag_c, uint8_t page_alert, char page_addr[MAC_ADDRESS_LEN]) // SWR future inputs go here
+{
+
+	printk("Notification, page_alert: %d\nDevice addr: %s\n", page_alert, page_addr);
+
+	if (!strcmp(page_addr, AR1_device))
+	{
+		nrf_gpio_pin_set(HAPTIC_MOTOR_PIN);
+		k_busy_wait(1000);
+		nrf_gpio_pin_clear(HAPTIC_MOTOR_PIN);
+		k_busy_wait(1000);
+		nrf_gpio_pin_set(HAPTIC_MOTOR_PIN);
+		k_busy_wait(1000);
+		nrf_gpio_pin_clear(HAPTIC_MOTOR_PIN);
+		
+	}
+	else if (!strcmp(page_addr, AR2_device))
+	{
+		nrf_gpio_pin_set(HAPTIC_MOTOR_PIN);
+		k_busy_wait(400);
+		nrf_gpio_pin_clear(HAPTIC_MOTOR_PIN);
+		k_busy_wait(200);
+		nrf_gpio_pin_set(HAPTIC_MOTOR_PIN);
+		k_busy_wait(200);
+		nrf_gpio_pin_clear(HAPTIC_MOTOR_PIN);
+		nrf_gpio_pin_set(HAPTIC_MOTOR_PIN);
+		k_busy_wait(200);
+		nrf_gpio_pin_clear(HAPTIC_MOTOR_PIN);
+		k_busy_wait(200);
+		nrf_gpio_pin_set(HAPTIC_MOTOR_PIN);
+		k_busy_wait(200);
+		nrf_gpio_pin_clear(HAPTIC_MOTOR_PIN);
+	}
+
+	return BT_GATT_ITER_CONTINUE;
+	// SWR things will happen
+}
+/*
+static void mtu_exchange_cb(struct bt_conn *conn, uint8_t err,
+							struct bt_gatt_exchange_params *params)
+{
+	printk("MTU exchange %u %s (%u)\n", bt_conn_index(conn),
+		   err == 0U ? "successful" : "failed", bt_gatt_get_mtu(conn));
+}
+
+static int mtu_exchange(struct bt_conn *conn)
+{
+	uint8_t conn_index;
+	int err;
+
+	conn_index = bt_conn_index(conn);
+
+	printk("MTU (%u): %u\n", conn_index, bt_gatt_get_mtu(conn));
+
+	mtu_exchange_params[conn_index].func = mtu_exchange_cb;
+
+	err = bt_gatt_exchange_mtu(conn, &mtu_exchange_params[conn_index]);
+	if (err)
+	{
+		printk("MTU exchange failed (err %d)", err);
+	}
+	else
+	{
+		printk("Exchange pending...");
+	}
+
+	return err;
+}*/
+
+
+static void discover_pag_completed(struct bt_gatt_dm *dm, void *ctx)
+{
+	int err;
+	struct bt_pag_client *pag = ctx;
+	char addr[MAC_ADDRESS_LEN]; // BT_ADDR_LE_STR_LEN];
+	struct bt_conn *conn = bt_pag_conn(pag);
+
+	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
+
+	printk("The discovery procedure succeeded\n");
+
+	bt_gatt_dm_data_print(dm);
+
+	err = bt_pag_handles_assign(dm, pag);
+	if (err)
+	{
+		printk("Could not init Paging client object (err %d)\n", err);
+		return;
+	}
+	else
+		printk("Paging Service handles are assgined.\n");
+
+	// assign the callback for notifications
+	struct bt_pag_client_cb_init init = {
+		.alert_cb = {
+			.pag_notify_cb = notify_func,
+		}};
+
+	// pass the callback to the client structure used in bt_page_alert_subscribe
+	bt_pag_cb_init(pag, &init);
+
+	err = bt_page_alert_subscribe(pag);
+	if (err)
+	{
+		printk("Subscribe failed (err %d)\n", err);
+	}
+	else
+	{
+		printk("[SUBSCRIBED]\n");
+	}
+
+	err = bt_gatt_dm_data_release(dm);
+	if (err)
+	{
+		printk("Could not release the discovery data (err %d)\n", err);
+	}
+	else
+		printk("Discorvery data released\n");
+
+	if (conn_count < CONFIG_BT_MAX_CONN)
+	{
+
+		err = bt_scan_start(BT_SCAN_TYPE_SCAN_ACTIVE);
+		if (err)
+		{
+			printk("Scanning failed to start (err %d)\n", err);
+		}
+		k_sleep(K_MSEC(500));
+	}
+	else
+		printk("Max devices connected");
+
+	// mtu_exchange(conn);
+}
+
+static void discover_pag_service_not_found(struct bt_conn *conn, void *ctx)
+{
+	printk("No more services\n");
+}
+
+static void discover_pag_error_found(struct bt_conn *conn, int err, void *ctx)
+{
+	printk("The discovery procedure failed, err %d\n", err);
+}
+
+static struct bt_gatt_dm_cb discover_pag_cb = {
+	.completed = discover_pag_completed,
+	.service_not_found = discover_pag_service_not_found,
+	.error_found = discover_pag_error_found,
+};
+
+static void scan_filter_match(struct bt_scan_device_info *device_info,
+							  struct bt_scan_filter_match *filter_match,
+							  bool connectable)
+{
+	int err;
+	char addr[MAC_ADDRESS_LEN]; // BT_ADDR_LE_STR_LEN];
+	struct bt_conn_le_create_param *conn_params;
+
+	bt_addr_le_to_str(device_info->recv_info->addr, addr, sizeof(addr));
+
+	printk("Filters matched. Address: %s connectable: %s\n",
+		   addr, connectable ? "yes" : "no");
+
+	err = bt_scan_stop();
+	if (err)
+	{
+		printk("Stop LE scan failed (err %d)\n", err);
+	}
+	else printk("Scanning stopped.\n");
+
+	conn_params = BT_CONN_LE_CREATE_PARAM(
+		BT_CONN_LE_OPT_CODED | BT_CONN_LE_OPT_NO_1M,
+		BT_GAP_SCAN_FAST_INTERVAL,
+		BT_GAP_SCAN_FAST_INTERVAL);
+
+	err = bt_conn_le_create(device_info->recv_info->addr, conn_params,
+							BT_LE_CONN_PARAM_DEFAULT,
+							&default_conn);
+
+	if (err)
+	{
+		printk("Create conn failed (err %d)\n", err);
+
+		err = bt_scan_start(BT_SCAN_TYPE_SCAN_ACTIVE);
+		if (err)
+		{
+			printk("Scanning failed to start (err %d)\n", err);
+			return;
+		}
+	}
+
+	printk("Connection pending\n");
+}
+
+BT_SCAN_CB_INIT(scan_cb, scan_filter_match, NULL, NULL, NULL);
+
+static void scan_init(void)
+{
+	int err;
+
+	/* Use active scanning and disable duplicate filtering to handle any
+	 * devices that might update their advertising data at runtime. */
+	struct bt_le_scan_param scan_param = {
+		.type = BT_LE_SCAN_TYPE_ACTIVE,
+		.interval = BT_GAP_SCAN_FAST_INTERVAL,
+		.window = BT_GAP_SCAN_FAST_WINDOW,
+		.options = BT_LE_SCAN_OPT_CODED | BT_LE_SCAN_OPT_NO_1M};
+
+	struct bt_scan_init_param scan_init = {
+		.connect_if_match = 1,
+		.scan_param = &scan_param,
+		.conn_param = NULL};
+
+	bt_scan_init(&scan_init);
+	bt_scan_cb_register(&scan_cb);
+
+	// SWR TODO change the filter type to short name and iterate through the device names
+	//* change this  for future pairing and un-pairing implementations
+	err = bt_scan_filter_add(BT_SCAN_FILTER_TYPE_UUID, BT_UUID_PAGING_SERVICE);
+	if (err)
+	{
+		printk("Scanning filters cannot be set (err %d)\n", err);
+
+		return;
+	}
+
+	err = bt_scan_filter_enable(BT_SCAN_UUID_FILTER, false);
+	if (err)
+	{
+		printk("Filters cannot be turned on (err %d)\n", err);
+	}
+}
+
+static void connected(struct bt_conn *conn, uint8_t conn_err)
+{
+	int err;
+	struct bt_conn_info info;
+	char addr[MAC_ADDRESS_LEN]; // BT_ADDR_LE_STR_LEN];
+
+	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
+
+	if (conn_err)
+	{
+		printk("Failed to connect to %s (%u)\n", addr, conn_err);
+
+		bt_conn_unref(default_conn);
+		default_conn = NULL;
+
+		err = bt_scan_start(BT_SCAN_TYPE_SCAN_ACTIVE);
+		if (err)
+		{
+			printk("Scanning failed to start (err %d)\n", err);
+		}
+
+		return;
+	}
+
+	err = bt_conn_get_info(conn, &info);
+
+	if (err)
+	{
+		printk("Failed to get connection info\n");
+	}
+	else
+	{
+		const struct bt_conn_le_phy_info *phy_info;
+
+		phy_info = info.le.phy;
+		printk("Connected: %s, tx_phy %u, rx_phy %u\n",
+			   addr, phy_info->tx_phy, phy_info->rx_phy);
+	}
+
+	if (conn == default_conn)
+	{
+
+		err = bt_gatt_dm_start(conn, BT_UUID_PAGING_SERVICE, &discover_pag_cb, &pag_c);
+		if (err)
+		{
+			printk("Failed to start discovery (err %d)\n", err);
+		}
+		else
+		{
+			// save the address of the newly added device
+			if (!strcmp(AR1_device, NOT_CONNECTED))
+			{
+				strcpy(AR1_device, addr);
+				printk("Added AR1_device at Address: %s\n", addr);
+			}
+			else if (!strcmp(AR2_device, NOT_CONNECTED))
+			{
+				strcpy(AR2_device, addr);
+				printk("Added AR2_device at Address: %s\n", addr);
+			}
+
+			conn_count++;
+			printk("Devices connected: %d\n", conn_count);
+
+		}
+	}
+}
+
+static void disconnected(struct bt_conn *conn, uint8_t reason)
+{
+
+	char addr[BT_ADDR_LE_STR_LEN];
+
+	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
+
+	printk("Disconnected: %s (reason 0x%02x)\n", addr, reason);
+
+	bt_conn_unref(conn);
+	printk("%s unreferenced\n", addr);
+
+	conn_count--;
+	printk("Devices connected: %d\n", conn_count);
+
+}
+
+BT_CONN_CB_DEFINE(conn_callbacks) = {
+	.connected = connected,
+	.disconnected = disconnected,
+};
+
+void offload_function(struct k_work *work_tem)
+{
+
+	int button_held_time = 0;
+	while (!nrf_gpio_pin_read(RECEIVER_BUTTON_PIN))
+	{
+		k_busy_wait(100000);
+		button_held_time++;
+
+		if (button_held_time > 40)//four seconds
+		{
+		
+			pm_state_force(0u, &(struct pm_state_info){PM_STATE_SOFT_OFF, 0, 0});
+
+			printk("Powering Off...\n");
+
+			k_busy_wait(2000000);//give the user time to release the button
+			
+		}
+	}
+		
+	printk("Work thread executed. \n");
+}
+
+static void button_handler(nrfx_gpiote_pin_t pin, nrfx_gpiote_trigger_t trigger, void *context)
+{
+	if (debounce_check)
+	{
+		printk("GPIO input event callback\n");
+
+		k_work_submit_to_queue(&offload_work_q, &my_work.work);
+			
+	}
+	debounce_check = !debounce_check;
+}
+
+nrfx_err_t configure_haptic_button(void)
+{
+    nrfx_err_t err = NRFX_SUCCESS;
+	uint8_t in_channel;
+
+
+    IRQ_CONNECT(DT_IRQN(DT_NODELABEL(gpiote)),
+		    DT_IRQ(DT_NODELABEL(gpiote), priority),
+		    nrfx_isr, nrfx_gpiote_irq_handler, 0);
+
+
+	err = nrfx_gpiote_init(0);
+	if (err != NRFX_SUCCESS) {
+		printk("nrfx_gpiote_init error: 0x%08X", err);
+		return err;
+	}
+
+	err = nrfx_gpiote_channel_alloc(&in_channel);
+	if (err != NRFX_SUCCESS) {
+		printk("Failed to allocate in_channel, error: 0x%08X", err);
+		return err;
+	}
+
+	/* Initialize input pin to generate event on high to low transition
+	 * (falling edge) and call button_handler()
+	 */
+	static const nrfx_gpiote_input_config_t input_config = {
+		.pull = NRF_GPIO_PIN_PULLUP,
+	};
+	const nrfx_gpiote_trigger_config_t trigger_config = {
+		.trigger = NRFX_GPIOTE_TRIGGER_HITOLO,
+		.p_in_channel = &in_channel,
+	};
+	static const nrfx_gpiote_handler_config_t handler_config = {
+		.handler = button_handler,
+	};
+
+	err = nrfx_gpiote_input_configure(RECEIVER_BUTTON_PIN,
+					  &input_config,
+					  &trigger_config,
+					  &handler_config);
+	if (err != NRFX_SUCCESS) {
+		printk("nrfx_gpiote_input_configure error: 0x%08X", err);
+		return err;
+	}
+
+    nrfx_gpiote_trigger_enable(RECEIVER_BUTTON_PIN, true);
+
+    return err;
+}
+
+void main(void)
+{
+	int err;
+
+	printk("~~~~~~~~~rareBit Receiver Demo~~~~~~~~~~~\n");
+
+	nrf_gpio_cfg_input(RECEIVER_BUTTON_PIN, NRF_GPIO_PIN_PULLUP);
+	nrf_gpio_cfg_output(HAPTIC_MOTOR_PIN);
+
+	/* Configure to generate PORT event (wakeup) on button 1 press. */
+	nrf_gpio_cfg_sense_set(RECEIVER_BUTTON_PIN, NRF_GPIO_PIN_SENSE_LOW);
+
+	int on_button_held = 0;
+	while (!nrf_gpio_pin_read(RECEIVER_BUTTON_PIN))
+	{
+		k_busy_wait(100000);
+		on_button_held++;
+
+		if (on_button_held > 40) break;//LED indicates
+
+		//TODO continue to select different modes, like DFU, 
+		//for on_button_held length values
+	}
+
+	if (on_button_held > 40)
+    {
+        printk("Initializing...\n");
+        k_busy_wait(2000000);
+    }
+	else
+	{
+		printk("Powering Off...\n");
+
+		pm_state_force(0u, &(struct pm_state_info){PM_STATE_SOFT_OFF, 0, 0});
+	    k_msleep(2000);
+
+	}
+
+	//Continues...
+	k_work_queue_start(&offload_work_q, my_stack_area,
+					K_THREAD_STACK_SIZEOF(my_stack_area), WORKQ_PRIORITY,
+					NULL);
+	strcpy(my_work.name, "Receiver Button Push thread");
+	k_work_init(&my_work.work, offload_function);
+	
+
+    //Initialize Button/Motor Peripherals
+    err = configure_haptic_button();
+    if (err != NRFX_SUCCESS)
+    {
+		printk("Haptic Button config failed (err %d)\n", err);
+        return;
+    } 
+
+	err = bt_enable(NULL);
+	if (err)
+	{
+		printk("Bluetooth init failed (err %d)\n", err);
+		return;
+	}
+
+	printk("Bluetooth initialized\n");
+
+	smp_bt_register();
+	//"E: Unable to register handle 0x0010"
+
+	scan_init();
+
+	err = bt_scan_start(BT_SCAN_TYPE_SCAN_ACTIVE);
+	if (err)
+	{
+		printk("Scanning failed to start (err %d)\n", err);
+		return;
+	}
+
+	printk("Scanning successfully started\n");
+
+	while(1)
+	{
+		k_msleep(300);
+	} 
+}
